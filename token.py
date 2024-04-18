d3c1853b697258085f74a553049d958544013c69
Massato01 <massatohc@gmail.com>
2024-04-18 02:21:26 -0300

    Init

---
+++ token.py
@@ -0,0 +1,49 @@
+### Dattebayo language
+
+## Integrantes
+# Antonio Gustavo Muniz     |   22.119.001-0
+# João Vitor Dias           |   22.119.006-9
+# Weverson da Silva         |   22.119.004-4
+
+## Keywords
+# if        | ninjutsu      | ^(ninjutsu)$
+# elif      | genjutsu      | ^(genjutsu)$
+# else      | taijutsu      | ^(taijutsu)$
+# for       | kagebunshin   | ^(kagebunshin)$
+# while     | tsukuyomi     | ^(tsukuyomi)$
+# functions | chakra        | ^(chakra)$
+# return    | kamui         | ^(kamui)$
+# print     | sharingan     | ^(sharingan)$
+# int       | rasengan      | ^(rasengan)$
+# float     | raikiri       | ^(raikiri)$
+# bool      | zetsu         | ^(zetsu)$
+# string    | kuchiyose     | ^(kuchiyose)$
+
+
+## Integer
+# ^(-|\+)?\d+$
+
+## Floating Point
+# ^(-|\+)?[0-9]\d*(.\d+)?$
+
+## Boolean
+# ^(True|False|0|1)
+
+## String
+# ^"[\s\S]*"$
+
+## Identifiers
+# ^([a-zA-Z])+(\w)*
+
+## Operators
+# +     | fuumashuriken  |       ^\+$
+# -     | kunai          |       ^\-$
+# *     | shuriken       |       ^\*$
+# /     | katana         |       ^\/$
+# =     | haku           |       ^=$
+# ==    | kirigakure     |       ^(==)$
+# &&    | kumogakure     |       ^(&&)$
+# ||    | amegakure      |       ^(||)$
+# \n    | ^\\n$
+# \t    | ^\\t$
+# ;     | ^\;$
