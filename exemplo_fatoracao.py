e82a115e7f8f83fe449f58a078cc21e40d5f32fa
Massato01 <massatohc@gmail.com>
2024-04-18 22:18:43 -0300

    NANAKI DÊ PULL

--- exemplo_fatoracao.py
+++ exemplo_fatoracao.py
@@ -1,7 +1,21 @@
 '''
+a)
 S -> Aa | b  |
 A -> Ac | Sd | e
 
-A' -> 
-A
+A' -> Ac | Aad | bd | e
+      α1   α2    β1   β2
+
+A'' -> bdA' | A'
+
+Regra final:
+A -> β1A' | β2A'
+A -> AcA | bdA | e
+
+________________________
+
+b)
+A -> aAB | aBc | aAc
+
+A -> aAB | aBc | aAc
 '''
